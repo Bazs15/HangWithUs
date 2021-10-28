87b8b4077cda4481f242d668356c39276647b3e2
bartokistvan <stevee1715@gmai.com>
2021-10-28 15:15:56 +0300

    replace

--- HangWithUs.py
+++ HangWithUs.py
@@ -1,5 +1,90 @@
 import random
 
+    
+def list_making(): 
+
+    word_list = open("c_and_c.txt", "r").readlines()
+    countries = []
+    capitals = []
+    for I in word_list:
+        c_and_c = I.split(" | ")
+        countries.append(c_and_c[0])
+        capitals.append(c_and_c[1].strip())
+    return countries
+
+#if __name__ == '__main__':
+        #main()
+
+
+
+
+#def play(word, lives):
+    #good_guess = True
+    #bad_guess = False
+    #lives = 6
+    #word_competition = "_" * len(countries)
+    #while bad_guess in word:
+        #print("Bad guess! ")
+        #lives -= 1
+    #if good_guess in word:
+        #print("Good guess! ")
+
+#def lines(word):
+    #list_for_lines = []
+    #for letter in word
+
+
+
+def choose_word(number_input):
+
+    short_list = []
+    long_list = []
+    countries = list_making() 
+    #chosen_s_word 
+    #chosen_l_word 
+
+
+    if number_input == "1":
+        for word in countries:
+            if len(word) <= 6:
+                short_list.append(word)
+        chosen_s_word = random.choice(short_list)
+        for letter in chosen_s_word:
+            chosen_s_word = chosen_s_word.replace(letter, '_')
+        #print(chosen_s_word)
+        return chosen_s_word
+    
+       
+    if number_input == "2":
+        for word in countries:
+            if len(word) > 6:
+                long_list.append(word)
+        chosen_l_word = random.choice(long_list)
+        for letter in chosen_l_word:
+            chosen_l_word = chosen_l_word.replace(letter, '_')
+        #print(chosen_l_word)
+        return chosen_l_word
+        
+
+        
+#def hide(chosen_word, guess):
+    #if guess in chosen_word:
+        #index = chosen_word.index(guess)
+        #if letter[index] = guess:
+            #print(letter.replace('_', guess)
+
+
+#def play(word, lives):
+    #good_guess = True
+    #bad_guess = False
+    #lives = 6
+    #word_competition = "_" * len(word)
+    #while bad_guess in word:
+        #print("Bad guess! ")
+        #lives -= 1
+    #if good_guess in word:
+        #print("Good guess! ")
+
 def main():
     print(
 """
@@ -17,65 +102,16 @@ def main():
 """)
 
     number_input = input("Please choose a level! Press 1 for easy, 2 for hard: ")
-    choose_word(number_input)
+    war = choose_word(number_input)
+    print(war)
 
-def list_making(): 
-
-    word_list = open("c_and_c.txt", "r").readlines()
-    countries = []
-    capitals = []
-    for I in word_list:
-        c_and_c = I.split(" | ")
-        countries.append(c_and_c[0])
-        capitals.append(c_and_c[1].strip())
-    return countries
 
 if __name__ == '__main__':
-        main()
+    main()
 
 
 
-
-#def play(word, lives):
-    #good_guess = True
-    #bad_guess = False
-    #lives = 6
-    #word_competition = "_" * len(word_lista)
-    #while bad_guess in word:
-        #print("Bad guess! ")
-        #lives -= 1
-    #if good_guess in word:
-        #print("Good guess! ")
-
-# def lines(word):
-#     list_for_lines = []
-#     for letter in word
-
-
-
-def choose_word(number_input):
-
-    short_list = []
-    long_list = []
-
-
-    if number_input == "1":
-        for word in countries:
-            if len(word) <= 6:
-                short_list.append(word)
-        chosen_word = random.choice(short_list)
-        return chosen_word
-
-    if number_input == "2":
-        for word in countries:
-            if len(word) > 6:
-                long_list.append(word)
-        chosen_word = random.choice(long_list)
-        return chosen_word
-
-
-
-        
+    
 
 
 #def display():
